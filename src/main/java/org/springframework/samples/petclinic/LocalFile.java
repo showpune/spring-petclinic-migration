0c5d8db617a7bc32bfddad7454c959c554bb7fe0
zhiyongli <iamlizhiyong@outlook.com>
2023-01-03 16:14:17 +0800

    Local file test

---
+++ src/main/java/org/springframework/samples/petclinic/LocalFile.java
@@ -0,0 +1,10 @@
+package org.springframework.samples.petclinic;
+
+import java.io.File;
+
+public class LocalFile {
+
+    public void test(){
+        File file = new File("c:\\temp\\d.text");
+    }
+}
