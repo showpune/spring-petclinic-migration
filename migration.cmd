3159a4b38c0ed283cb820ec4e0ef64646a1df9e0
zhiyongli <iamlizhiyong@outlook.com>
2023-01-16 14:43:18 +0800

    Test Script

--- migration.cmd
+++ migration.cmd
@@ -1,1 +1,1 @@
-./mvnw org.openrewrite.maven:rewrite-maven-plugin:run -Drewrite.activeRecipes=com.azure.spring.migration.UpgradeToAzureSpringApps -Drewrite.recipeArtifactCoordinates=org.openrewrite.recipe:rewrite-spring:4.31.0
+Run ./mvnw -Pgithub org.openrewrite.maven:rewrite-maven-plugin:4.31.0:run "-Drewrite.activeRecipes=com.azure.spring.migration.UpgradeToAzureSpringApps" "-Drewrite.recipeArtifactCoordinates=com.azure.spring.migration:azure-spring-rewrite:0.1.0,org.openrewrite.recipe:rewrite-spring:4.31.0
