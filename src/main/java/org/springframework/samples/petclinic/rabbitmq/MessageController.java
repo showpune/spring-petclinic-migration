f0bd50d8ab2e219f921df6e0dd5a449b25dd514d
showpune <iamlizhiyong@outlook.com>
2024-08-28 15:57:09 +0800

    demo ready

--- src/main/java/org/springframework/samples/petclinic/rabbitmq/MessageController.java
+++ src/main/java/org/springframework/samples/petclinic/rabbitmq/MessageController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageController {
 
+	@Autowired
 	private final AmqpTemplate amqpTemplate;
 
-	@Autowired
 	public MessageController(AmqpTemplate amqpTemplate) {
 		this.amqpTemplate = amqpTemplate;
 	}
