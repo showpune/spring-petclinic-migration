d116d99684c2b929702303264367d7b6ac3e3518
zhiyongli <iamlizhiyong@outlook.com>
2025-02-13 11:47:13 +0800

    lsp

--- runlsp1.sh
+++ runlsp1.sh
@@ -1,4 +1,4 @@
-podman run -v $(pwd)/result-in-one.yaml:/analyzer-lsp/output.yaml:Z \
+docker run -v $(pwd)/result-in-one.yaml:/analyzer-lsp/output.yaml:Z \
            -v $(pwd)/provider_settings.json:/analyzer-lsp/provider_settings.json \
            -v $(pwd)/rule-in-one.yaml:/analyzer-lsp/rule-example.yaml \
            -v $(pwd):/analyzer-lsp/examples/spring-petclinic \
